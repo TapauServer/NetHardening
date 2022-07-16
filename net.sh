bd70bd136e68fd08eb452bc11c0c5cf60f989097
KerisPutih <106005322+TamingSari1337@users.noreply.github.com>
2022-07-16 20:27:12 +0800

    Add files via upload

---
+++ net.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+#color
+White="\033[37m"
+Red="\033[91m"
+Blue="\033[94m"
+
+#Tools
+menu(){
+	clear
+	echo -e -n $Blue;figlet "NetHardening"
+	echo ""
+	echo -e $White
+	echo -e "By TamingSari (BETA VERSION)"
+	echo ""
+	echo -e "1. Enable"
+	echo -e "2. Disable"
+	mainmenu
+}
+mainmenu(){
+	echo ""
+	echo -e -n "Select > ";read sel
+	if [ $sel == "1" ];then
+		sysctl -w net.ipv4.conf.default.rp_filter=1 &> /dev//null
+		sysctl -w net.ipv4.conf.all.rp_filter=1 &> /dev//null
+		sysctl -w net.ipv4.tcp_syncookies=1 &> /dev//null
+		sysctl -w net.ipv4.ip_forward=0 &> /dev//null
+		sysctl -w net.ipv6.conf.all.forwarding=0 &> /dev//null
+		sysctl -w net.ipv4.conf.all.accept_redirects=0 &> /dev//null
+		sysctl -w net.ipv6.conf.all.accept_redirects=0 &> /dev//null
+		sysctl -w net.ipv4.conf.all.secure_redirects=1 &> /dev//null
+		sysctl -w net.ipv4.conf.all.send_redirects=0 &> /dev//null
+		sysctl -w net.ipv4.conf.all.accept_source_route=0 &> /dev//null
+		sysctl -w net.ipv6.conf.all.accept_source_route=0 &> /dev//null
+		sysctl -w net.ipv4.conf.all.log_martians=1 &> /dev//null
+		echo -e "hardening successfully configured"
+	elif [ $sel == "2" ];then
+		sysctl -w net.ipv4.conf.default.rp_filter=1 &> /dev//null
+		sysctl -w net.ipv4.conf.all.rp_filter=1 &> /dev//null
+		sysctl -w net.ipv4.tcp_syncookies=1 &> /dev//null 
+		sysctl -w net.ipv4.ip_forward=0 &> /dev//null 
+		sysctl -w net.ipv6.conf.all.forwarding=1 &> /dev//null 
+		sysctl -w net.ipv4.conf.all.accept_redirects=0 &> /dev//null
+		sysctl -w net.ipv6.conf.all.accept_redirects=0 &> /dev//null
+		sysctl -w net.ipv4.conf.all.secure_redirects=1 &> /dev//null
+		sysctl -w net.ipv4.conf.all.send_redirects=0 &> /dev//null
+		sysctl -w net.ipv4.conf.all.accept_source_route=0 &> /dev//null
+		sysctl -w net.ipv6.conf.all.accept_source_route=0 &> /dev//null
+		sysctl -w net.ipv4.conf.all.log_martians=1 &> /dev//null
+		echo -e "hardening was successfully changed to default"
+
+	fi
+}
+
+menu
